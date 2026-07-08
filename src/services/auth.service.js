0248522623c07d1d040a7db8898be12adc695f70
Reynaldo Cruz <rjosecruz@gmail.com>
2026-07-07 23:29:52 -0600

    refac: -se agregan modulos jwt y password al auth.service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,2 +1,12 @@
-const userRepository=require('../repositories/user.repository')
-const RefreshTokenRepository=require('../repositories/refreshToken.repository')
+const userRepository = require('../repositories/user.repository')
+const RefreshTokenRepository = require('../repositories/refreshToken.repository')
+
+const {
+    hashPassword,
+    comparePassword
+} = require('../utils/password')
+const {
+    generateAccessToken,
+    generateRefreshToken,
+    verifyRefreshToken
+} = require('../utils/jwt')
